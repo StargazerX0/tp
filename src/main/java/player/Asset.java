762517670628ba296e9e22f390121b7af5fa29e3
CXIA17 <E0985916@u.nus.edu>
2024-04-09 23:38:56 +0800

    test errors fixed

--- src/main/java/player/Asset.java
+++ src/main/java/player/Asset.java
@@ -25,7 +25,7 @@ public class Asset {
     private int totalAsset;
 
     public Asset() {
-        this.totalAsset = 500000;
+        this.totalAsset = 5000;
     }
 
     public Asset(int totalAsset) {
