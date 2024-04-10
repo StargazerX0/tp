d76462242835fe0f2b182d8526429aa52ae681ea
StargazerX0 <lkjsgu@outlook.com>
2024-04-10 17:47:04 +0800

    Add Hangman Game to exercise

--- src/main/java/command/ExerciseCommand.java
+++ src/main/java/command/ExerciseCommand.java
@@ -1,16 +1,49 @@
 package command;
 
 import exception.InvalidMarkException;
+import minigame.Hangman;
 import minigame.TicTacToe;
 import player.PlayerProfile;
 import ui.ResponseManager;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExerciseCommand implements Command {
 
     public void execute(PlayerProfile playerProfile) {
+        ResponseManager.indentPrint("Choose your game:\n1. Tic Tac Toe\n2. Hangman\n");
+        int gameChoice = getGameChoice();
 
+        if (gameChoice == 1) {
+            playTicTacToe(playerProfile);
+        } else if (gameChoice == 2) {
+            playHangman(playerProfile);
+        }
+    }
+
+    private int getGameChoice() {
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+
+        while (true) {
+            try {
+                ResponseManager.indentPrint("Enter your choice (1 for Tic Tac Toe, 2 for Hangman):\n");
+                choice = scanner.nextInt();
+
+                if (choice == 1 || choice == 2) {
+                    return choice;
+                } else {
+                    ResponseManager.indentPrint("Invalid choice, please select 1 for Tic Tac Toe or 2 for Hangman\n");
+                }
+            } catch (InputMismatchException ime) {
+                ResponseManager.indentPrint("Please enter a valid integer.\n");
+                scanner.nextLine(); // Consume the invalid input
+            }
+        }
+    }
+
+    private void playTicTacToe(PlayerProfile playerProfile) {
         ResponseManager.indentPrint("Please choose your mark: X or O\n");
         char playerMark = '-';
         while ((playerMark != 'X') && (playerMark != 'O')) {
@@ -29,14 +62,23 @@ public class ExerciseCommand implements Command {
         }
     }
 
+    public void playHangman(PlayerProfile playerProfile) {
+        Hangman game = new Hangman();
+        game.startGame();
+        if (game.getStatus() == 1) {
+            playerProfile.addHealth(10);
+        } else if (game.getStatus() == -1) {
+            playerProfile.loseHealth(5);
+        }
+    }
+
     public char getMark() throws InvalidMarkException {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         if (!input.equals("X") && !input.equals("O")) {
             throw new InvalidMarkException("Mark can only be X or O\n");
         }
-        char playerMark = input.charAt(0);
-        return playerMark;
+        return input.charAt(0);
     }
 
     public boolean isExit() {
---
+++ src/main/java/minigame/Hangman.java
@@ -0,0 +1,104 @@
+package minigame;
+
+import ui.ResponseManager;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Hangman implements MiniGame {
+    private static final String[] WORDS = {
+        "java", "computer", "program", "keyboard", "mouse",
+        "algorithm", "software", "hardware", "network", "database",
+        "security", "coding", "development", "design", "architecture",
+        "interface", "application", "system", "technology", "internet",
+        "server", "client", "cloud", "virtual", "binary",
+        "function", "variable", "class", "object", "method",
+        "loop", "condition", "array", "list", "string",
+        "integer", "boolean", "char", "exception", "thread",
+        "cs2113"
+    };
+
+    private String chosenWord;
+    private Set<Character> guessedLetters;
+    private int round;
+    private int totalWrongGuesses;
+
+    public Hangman() {
+        round = 0;
+        totalWrongGuesses = 0;
+    }
+
+    private void initializeGame() {
+        int randomIndex = (int) (Math.random() * WORDS.length);
+        chosenWord = WORDS[randomIndex];
+        guessedLetters = new HashSet<>();
+
+        char letterToHide = chosenWord.charAt((int) (Math.random() * chosenWord.length()));
+        for (char c : chosenWord.toCharArray()) {
+            if (c != letterToHide) {
+                guessedLetters.add(c);
+            }
+        }
+    }
+
+    private void printWordState() {
+        StringBuilder display = new StringBuilder();
+        for (char letter : chosenWord.toCharArray()) {
+            if (guessedLetters.contains(letter)) {
+                display.append(letter).append(" ");
+            } else {
+                display.append("_ ");
+            }
+        }
+        ResponseManager.indentPrint(display.toString() + "\n");
+    }
+
+    public int getStatus() {
+        if (totalWrongGuesses >= 3) {
+            return -1;
+        } else if (round == 5) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public void startGame() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (round < 5 && totalWrongGuesses < 3) {
+            initializeGame();
+            ResponseManager.indentPrint("Round " + (round + 1) + ":\n");
+            printWordState();
+
+            ResponseManager.indentPrint("Guess the missing letter:\n");
+            String input = scanner.nextLine().toLowerCase();
+            if (input.length() == 1) {
+                char guessedChar = input.charAt(0);
+                if (chosenWord.indexOf(guessedChar) >= 0 && !guessedLetters.contains(guessedChar)) {
+                    guessedLetters.add(guessedChar);
+                    printWordState();
+                    ResponseManager.indentPrint("Correct! Moving to next round.\n");
+                } else {
+                    totalWrongGuesses++;
+                    ResponseManager.indentPrint("Wrong guess!\n");
+                }
+            } else {
+                ResponseManager.indentPrint("Please guess one letter at a time.\n");
+            }
+
+            round++;
+        }
+
+        outputResult(); 
+    }
+
+    public void outputResult() {
+        if (totalWrongGuesses >= 3) {
+            ResponseManager.indentPrint("Game over! Too many wrong guesses.\n");
+        } else if (round == 5) {
+            ResponseManager.indentPrint("Game completed! You finished all rounds.\n");
+        }
+    }
+}
+
