b17b1247236a2cba5c493bd4d13d7d3374d58b85
ZMinghuiZ <zmh992545@gmail.com>
2024-04-14 16:26:10 +0800

    Adjust Code structure

--- src/main/java/command/CheckStatusCommand.java
+++ src/main/java/command/CheckStatusCommand.java
@@ -1,1 +1,18 @@
+package command;
 
+import player.PlayerProfile;
+import static ui.ResponseManager.indentPrint;
+
+class CheckStatusCommand implements Command {
+    public void execute(PlayerProfile playerProfile) {
+        indentPrint("Current Status:" + playerProfile.toString());
+    }
+
+    public boolean isExit() {
+        return false;
+    }
+
+    public boolean canGenerateEvent() {
+        return false;
+    }
+}
--- src/main/java/command/ExitCommand.java
+++ src/main/java/command/ExitCommand.java
@@ -1,12 +1,7 @@
 package command;
 
-import player.PlayerProfile;
-import ui.ResponseManager;
-
 public class ExitCommand implements Command {
-    public void execute(PlayerProfile playerProfile) {
-        ResponseManager.printGoodbye();
-    }
+
 
     public boolean isExit() {
         return true;
