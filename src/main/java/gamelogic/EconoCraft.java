0de1b00a5be67cd7d13af456ec656a17ee09b2b6
ZMinghuiZ <zmh992545@gmail.com>
2024-04-14 21:28:54 +0800

    Add JavaDoc for main and gameLogic

--- src/main/java/gamelogic/EconoCraft.java
+++ src/main/java/gamelogic/EconoCraft.java
@@ -1,5 +1,8 @@
 package gamelogic;
 
+/**
+ * Represents the main entry point of the game.
+ */
 public class EconoCraft {
     public static void main(String[] args) {
         EconoCraftLogic.initializeGame()
