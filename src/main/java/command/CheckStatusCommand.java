a90a3003de96bfcc44b31f51084555dab29f35b2
ZMinghuiZ <zmh992545@gmail.com>
2024-04-14 16:20:24 +0800

    Solves CI

--- src/main/java/command/CheckStatusCommand.java
+++ src/main/java/command/CheckStatusCommand.java
@@ -1,18 +1,1 @@
-package command;
 
-import player.PlayerProfile;
-import static ui.ResponseManager.indentPrint;
-
-public class CheckStatusCommand implements Command {
-    public void execute(PlayerProfile profile) {
-        indentPrint("Current Status:\n" + profile.toString());
-    }
-
-    public boolean isExit() {
-        return false;
-    }
-
-    public boolean canGenerateEvent() {
-        return false;
-    }
-}
